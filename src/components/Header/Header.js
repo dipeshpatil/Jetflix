f5e5241b7bc75eb59af23d000c017521cad7f0ee
Dipesh Patil <heydipesh28@gmail.com>
2021-07-04 00:37:18 +0530

    added fancy svg animation for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,25 @@
 import "./Header.css";
+import MaskedStyleSVG from "./maskedShape.svg";
+
+const maskStyles = {
+  margin: "0 auto",
+  textAlign: "center",
+  backgroundImage: `url(${MaskedStyleSVG})`,
+  backgroundSize: "cover",
+  backgroundPosition: "50%",
+  backgroundRepeat: "no-repeat",
+  width: "auto",
+  backgroundClip: "text",
+  WebkitBackgroundClip: "text",
+  color: "transparent",
+};
 
 const Header = () => {
   return (
-    <span onClick={() => window.scroll(0, 0)} className="header">
-      🎬 Jetflix 🎥
+    <span onClick={() => window.scroll(0, 0)} className="header maskText">
+      <span className="" style={maskStyles}>
+        Jetflix
+      </span>
     </span>
   );
 };
