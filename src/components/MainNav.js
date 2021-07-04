c3aa4a972673e95eb7b00e5740fbd2ce4873e52d
Dipesh Patil <heydipesh28@gmail.com>
2021-07-04 06:40:12 +0530

    reverted color

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles({
     position: "fixed",
     bottom: 0,
     backgroundColor: isDark ? "#000" : "#fff",
-    boxShadow: "0px 2px 15px #3742fa",
+    boxShadow: "0px 2px 15px #EA2027",
     zIndex: 100,
   },
 });
